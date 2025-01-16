b96f8143ad8e2a349f58e1849af095db19161269
melnard <dejesusmelnard@gmail.com>
2025-01-17 01:32:51 +0800

    update from astro 4.0 to 5.0

---
+++ src/content.config.ts
@@ -0,0 +1,44 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const projectsCollections = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/data/projects' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      author: z.string(),
+      image: z.object({
+        src: image(),
+        alt: z.string(),
+      }),
+      description: z.string(),
+      technology: z.array(z.string()),
+      tags: z.array(z.string()),
+      link: z.string(),
+      repo: z.string(),
+      shortDesc: z.string(),
+    }),
+});
+
+const blogCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/data/blog' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      author: z.string(),
+      image: z.object({
+        src: image(),
+        alt: z.string(),
+      }),
+      description: z.string(),
+      technology: z.array(z.string()),
+      tags: z.array(z.string()),
+    }),
+});
+
+export const collections = {
+  projects: projectsCollections,
+  blog: blogCollection,
+};
