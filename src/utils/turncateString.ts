e7f78120c1659d85c729715349701f2bc366e7f2
melnard <dejesusmelnard@gmail.com>
2025-01-16 08:49:25 +0800

    change logo

---
+++ src/utils/turncateString.ts
@@ -0,0 +1,8 @@
+function truncateString(str: string, maxLength: number): string {
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return str.slice(0, maxLength) + '...';
+}
+
+export default truncateString;
